ce7dee75e274b9dd8452910ae4f656c7790122ca
Lavanya Gawas <lavanyagawas07@gmail.com>
2026-07-09 10:49:33 +0530

    Remove obsolete session management script

--- fix_sessions.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn = sqlite3.connect(r'D:\UFAMeasy_Machine_Server\server\data\params.db')
-conn.execute("UPDATE sessions SET status='ended', ended_at=datetime('now') WHERE status='running'")
-conn.commit()
-print(conn.execute('SELECT status, COUNT(*) FROM sessions GROUP BY status').fetchall())
